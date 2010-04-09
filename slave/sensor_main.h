9bf4d69c36813c43e0810926a2bf0cb7494a32e6
Markus Klein <m.klein@mfc-linz.at>
2010-04-09 23:16:36 +0200

    Erste Modbus-Implementierung laut Standard. Fehlererkennung und senden von Fehlercodes. Funktion 0x04 Read Register implementiert.

---
+++ slave/sensor_main.h
@@ -0,0 +1,65 @@
+#ifndef _sensor_main_h_
+#define _sensor_main_h_
+
+typedef struct {
+	uint16_t transmId;
+	uint16_t protoId;
+	uint16_t length;
+	uint8_t unitId;
+} mbap_t;
+
+typedef struct {
+	uint8_t function;
+	uint16_t start_address;
+	uint16_t amount;
+} request_t;
+
+typedef struct {
+	uint8_t function;
+	uint8_t bytecount;
+} response_t;
+
+typedef struct {
+	uint8_t function;
+	uint8_t errorcode;
+} error_t;
+
+// Modbus Message incl. MBAP and PDU
+typedef struct {
+	mbap_t mbap;
+	union {
+		request_t request;
+		response_t response;
+		error_t error;
+	};
+} modbusmsg_t;
+
+// Modbus error codes
+#define MB_ERR_FUNC 1
+#define MB_ERR_ADDR 2
+#define MB_ERR_AMOUNT 3
+#define MB_ERR_PROC 4
+
+#define MB_FUNC_READREG 0x04
+
+/*
+   The following macros are used to convert from node 
+   byteorder (little endian) to MODBUS byteorder (big endian)
+*/
+#define CONVBYTEORDER_W(intype, outtype, input) \
+({                                                  \
+	intype __param = (intype) input;            \
+	outtype __result;                           \
+	__asm__ volatile(                           \
+                "mov %A0, %B1" "\n\t"               \
+		"mov %B0, %A1" "\n\t"               \
+		: "=&r" (__result)                  \
+		: "r" (__param)                     \
+	);                                          \
+	__result;                                   \
+})
+
+#define TO_UINT16(input) CONVBYTEORDER_W(uint16_t, uint16_t, input)
+#define FROM_UINT16(input) CONVBYTEORDER_W(uint16_t, uint16_t, input)
+
+#endif
